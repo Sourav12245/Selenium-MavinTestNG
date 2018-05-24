fabf4f7a31e6178600610015483c163e2622cee9
Sourav12245 <joinghoshcompany@gmail.com>
2018-05-25 02:41:57 +0530

    25.05.2018

---
+++ logmain/src/main/java/logcheck/logmain/App.java
@@ -0,0 +1,13 @@
+package logcheck.logmain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
